f0df444885d6951b9efa03ae0f51ab307ee71227
Jason Cable <cablej02@gmail.com>
2024-10-08 20:58:12 -0500

    work on scramble game and home page

--- assets/js/word-game.js
+++ assets/js/word-game.js
@@ -42,7 +42,7 @@ const keyboardLayout = [
     ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
     ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'],
     ['DELETE','Z', 'X', 'C', 'V', 'B', 'N', 'M','ENTER'],
-  ];
+];
 
 const keyboardContainer = document.createElement('div');
 keyboardContainer.id = 'keyboard-container';
