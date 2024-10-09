3252984a1961c66fbfe7d404e52a335237ebbea2
Nattan Aguia <nattan23@msn.com>
2024-10-09 19:57:38 -0400

    wordle.js

--- assets/js/wordle.js
+++ assets/js/wordle.js
@@ -1,7 +1,9 @@
 const guessContainerEl = document.getElementById('guess-container');
 
 const currentGame = {
-    
+    solution: 'banana',
+    guesses: ['toggle'],
+    maxGuesses: 6
 }
 
 /* UI Functions */
@@ -10,17 +12,21 @@ const generateBoard = (numGuesses, wordLength) => {
     guessContainerEl.innerHTML = '';
 
     for(let i = 0; i < numGuesses; i++) {
-        //i=0
         const guessRowEl = document.createElement('div');
-        guessRowEl.classList.add('guess');
+        guessRowEl.className = 'row justify-content-center';
 
         for(let j = 0; j < wordLength; j++){
             const letterBoxEl = document.createElement('div');
-            letterBoxEl.classList.add('guess');
+            letterBoxEl.className = 'col-1 border border-3 text-center';
+            letterBoxEl.textContent = 'A';
             guessRowEl.appendChild(letterBoxEl)
         }
         guessContainerEl.appendChild(guessRowEl);
     }
+}
+
+const changeLetterColor = () => {
+    
 }
 
 
