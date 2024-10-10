e71871ca67484ab84b20aeb57a69745e3a6d1659
Jason Cable <cablej02@gmail.com>
2024-10-10 13:47:56 -0500

    added guess handling functionality

--- assets/js/wordle.js
+++ assets/js/wordle.js
@@ -2,9 +2,25 @@ const guessContainerEl = document.getElementById('guess-container');
 const bodyEl = document.querySelector('body');
 
 const currentGame = {
-    solution: 'banana',
-    guesses: ['toggle'],
-    maxGuesses: 6,
+    solution: null,
+    guesses: [],
+    maxGuesses: null,
+    addGuess(guess){
+        this.guesses.push(guess);
+        saveCurrentGame();
+    },
+    setNewGame(solution,maxGuesses){
+        this.solution = solution;
+        this.maxGuesses = maxGuesses;
+        this.guesses = [];
+        saveCurrentGame();
+    },
+    clearGame(){
+        this.solution = null;
+        this.maxGuesses = null;
+        this.guesses = [];
+        saveCurrentGame();
+    }
 }
 
 /* UI Functions */
@@ -26,8 +42,12 @@ const generateBoard = (numGuesses, wordLength) => {
     }
 }
 
-const changeLetterColor = () => {
+const displayLetterColorChange = () => {
     
+}
+
+const displayGuess = (guess) => {
+
 }
 
 const displayGuessWordColor = (guess) => {
@@ -57,14 +77,44 @@ const handleKeyPress = (key) => {
 }
 
 const handleGuess = (guess) => {
+    if (guess.length === currentGame.solution.length) {
+        if (guess === currentGame.solution){
+            console.log('Correct word:', guess);
+            handleGameOver(true);
+        }else{
+            if(isValidWord(guess)){
+                console.log(`Valid word: ${guess}`);
+                currentGame.addGuess(guess);
+
+                //TODO: Display ui response
+            }else {
+                console.log(`Invalid word: ${guess}`);
+                //TODO: Display invalid word message
+            }
+        }
+    }else{
+        //TODO: Display a not enough letters message
+    }
+
     // currentGame.guesses.push(guess);
     // console.log('Guess:', guess);
     // console.log('Guesses:', currentGame.guesses);
 }
 
-/* Data Functions */
-const saveCurrentGame = () => localStorage.setItem('wordleData',JSON.stringify(currentGame));
+const handleGameOver = (winBool) => {
+    const solution = currentGame.solution;
+    currentGame.clearGame();
 
+    if(winBool){
+        console.log('You Win!');
+    }else{
+        console.log(`You Lose! The solution word was: ${solution}`);
+    }
+}
+
+/* Data Functions */
+const saveCurrentGame = () => localStorage.setItem('wordleCurGame',JSON.stringify(currentGame));
+const loadCurrentGame = () => localStorage.getItem('wordleCurGame') || null;
 
 
 /* Event Listeners */
