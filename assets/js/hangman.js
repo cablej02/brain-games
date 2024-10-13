720c90cdd8ee8b65d3fd894f87ab24c4f188ff46
Nattan Aguia <nattan23@msn.com>
2024-10-13 17:17:28 -0400

    addition of hangman js 2.0

--- assets/js/hangman.js
+++ assets/js/hangman.js
@@ -12,11 +12,11 @@ const currentGame = (() => {
     let solution = null;
     const guesses = [];
     let maxGuesses = null;
-    const save = () =>{
+    const saveData = () =>{
         const curGameHelper = {solution,guesses,maxGuesses}
         localStorage.setItem('hangmanCurrentGame', JSON.stringify(curGameHelper));
     }
-    const load = () =>{
+    const loadData = () =>{
         const data = JSON.parse(localStorage.getItem('hangmanCurrentGame') || null);
         if(data){
             solution = data.solution;
@@ -28,25 +28,28 @@ const currentGame = (() => {
     }
     const addGuess = (guess) => {
         guesses.push(guess);
-        save();
+        saveData();
     }
     const setNewGame = (newSolution,newMaxGuesses) => {
         solution = newSolution;
         maxGuesses = newMaxGuesses;
         guesses.length = 0;
-        save();
+        saveData();
     }
     const clearGame = () => {
         solution = null;
         maxGuesses = null;
         guesses.length = 0;
-        save();
+        saveData();
     }
     return{
         addGuess,
         setNewGame,
         clearGame,
-        load
+        loadData,
+        getSolution: () => solution,
+        getGuesses: () => [...guesses],
+        getMaxGuesses: () => maxGuesses
     }
 })();
 
@@ -55,8 +58,6 @@ const displayRevealedLetter = (letter) => {
     const span = document.createElement('span');
     span.textContent = letter;
     guessContainerEl.appendChild(span);
-    currentGame.guesses
-
 }
 
 
@@ -71,9 +72,9 @@ const handleKeyPress = (key) => {
     
     //TODO: Fill in logic of this function
     if (alphabet.includes(k)) {
-        if (!currentGame.guesses.includes(k)) {
+        if (!currentGame.getGuesses().includes(k)) {
             currentGame.addGuess(k);
-            if (currentGame.solution.includes(k)){  
+            if (currentGame.getSolution().includes(k)){  
                 keyboard.setKeyColorGreen(k);              
                 console.log(generateDisplayWord());
             } else {
@@ -85,27 +86,23 @@ const handleKeyPress = (key) => {
 
 
 const generateDisplayWord = () =>{
-    const solution = currentGame.solution;
-    const guesses = [...currentGame.guesses];
+    const solution = currentGame.getSolution();
+    const guesses = currentGame.getGuesses();
     let displayWord = solution.split('').map(letter => guesses.includes(letter) ? letter : '_').join('');
-
     return displayWord;
 }
 
 const startNewGame = () =>{
-    let solution = wordList.getRandomWord(Math.floor(Math.random)*6 + 5); //TODO: make this a user input to choose word length, or randomize
+    let solution = wordList.getRandomWord(Math.floor(Math.random)*6 + 5);
     currentGame.setNewGame(solution, 5); //TODO: maybe put this in a static variable
-    console.log(solution);
+    console.log(solution); //TODO: remove this eventually maybe
 
     setUI();
 }
 
 const handleGameOver = (winBool) =>{
-    
+    //TODO: this function
 }
-
-//data functions
-
 
 //Event Listeners
 bodyEl.addEventListener('keydown', (event) => handleKeyPress(event.key));
@@ -114,6 +111,11 @@ bodyEl.addEventListener('keydown', (event) => handleKeyPress(event.key));
 wordList.loadWords().then(() => {
     keyboard.initialize(false);
 
-    //TODO: Implement loading of saved game/starting a new game
-    startNewGame();
+    currentGame.loadData();
+
+    if(currentGame.getSolution() === null){
+        startNewGame();
+    }else{
+        setUI();
+    }
 });
