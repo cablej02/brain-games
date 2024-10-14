a7d9a3721ccb588512434a1e74b4f942ee403bf0
Jordan Lorts <jordanjlorts@gmail.com>
2024-10-14 11:44:12 -0500

    adding larger grid

--- assets/js/matchGame.js
+++ assets/js/matchGame.js
@@ -1,79 +1,87 @@
-//delete commments, only using to keep me organized
+const gameBoard = document.getElementById('game-board');
+let tiles = [];
+let flippedTiles = [];
+let matchedPairs = 0;
+const tileValues = [];
 
-const gameBoard = document.getElementById('game-board'); 
-let tiles = []; 
-let flippedTiles = []; 
-let matchedPairs = 0; 
-const tileValues = []; 
-
-// Paired tiles
-for (let i = 1; i <= 2; i++) { // start at 1 and repeat until 2, i++ increments up by 1 
-    tileValues.push(i, i); // this array has 2 of the same number being push, essentially the same number twice
+for (let i = 1; i <= 8; i++) {
+    tileValues.push(i, i);
 }
 
-function shuffle(array) { //Fisher-Yates shuffle src = wikipedia
+function shuffle(array) {
     for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1)); 
+        const j = Math.floor(Math.random() * (i + 1));
         [array[i], array[j]] = [array[j], array[i]];
     }
-    return array; 
-} // yoinked this. get what it does, still understanding how and iff it works
-
-shuffle(tileValues); 
-
-for (let i = 0; i < tileValues.length; i++) { 
-    const tile = document.createElement('div'); // tiles
-    tile.classList.add('tile'); //add class
-    tile.dataset.value = tileValues[i]; //assigns value to tile from the value of the tileValue duh 
-    tile.innerHTML = '<span class="hidden">' + tileValues[i] + '</span>'; //hides tiles when palced
-    tile.addEventListener('click', flipTile); //need to create a fliped tile function
-    tiles.push(tile); 
-    gameBoard.appendChild(tile); //puts in the gameBoard
+    return array;
 }
 
+function startGame() {
+    shuffle(tileValues);
+    matchedPairs = 0; 
+    tiles.forEach(tile => gameBoard.removeChild(tile)); 
+    tiles = []; 
 
-// Flipping a tile
+    for (let value of tileValues) {
+        const tile = document.createElement('div');
+        tile.classList.add('tile');
+        tile.dataset.value = value;
+        tile.innerHTML = '<span class="hidden">' + value + '</span>'; 
+        tile.addEventListener('click', flipTile);
+        tiles.push(tile);
+        gameBoard.appendChild(tile);
+    }
+}
+
+function resetGame() {
+    matchedPairs = 0;
+    flippedTiles = []; 
+    tiles.forEach(tile => {
+        tile.classList.remove('flipped', 'matched');
+        tile.querySelector('span').classList.add('hidden'); 
+    });
+}
+
 function flipTile() {
     if (flippedTiles.length === 2 || this.classList.contains('flipped') || this.classList.contains('matched')) {
         return;
-    } // check conditions 1. 2 tiles flipped, no actions 2. checks if tile is flipped ALREADY 
-    //3. does not allow matched tiles to "flip" again
+    }
     
-    this.classList.add('flipped'); //adds class "flipped" 
-    this.querySelector('span').classList.remove('hidden'); //checks for "span" element (value) and removes "hidden" class
-    flippedTiles.push(this); //adding any flipped tile to "this" array
+    this.classList.add('flipped');
+    this.querySelector('span').classList.remove('hidden');
+    flippedTiles.push(this);
 
     if (flippedTiles.length === 2) {
-        checkForMatch(); //if 2 tiles flipped then calls the checkForMatch function
+        checkForMatch();
     }
 }
 
-// Check if the flipped tiles match
 function checkForMatch() {
-    const [tile1, tile2] = flippedTiles; // taking the array flippedTiles and assigning tile1 and tile2
+    const [tile1, tile2] = flippedTiles;
 
-    if (tile1.dataset.value === tile2.dataset.value) { // checks to see value of tile
+    if (tile1.dataset.value === tile2.dataset.value) {
         tile1.classList.add('matched');
         tile2.classList.add('matched');
         matchedPairs++;
-        if (matchedPairs === 2) { //number of matches to win. needs to change with number of tiles/2
-            alert('You won! All pairs matched.'); //need new "alert" cant be alert or prompt
-        } //check bootstrap
+        if (matchedPairs === 8) {
+            alert('You won! All pairs matched.');
+        }
     } else {
         setTimeout(() => {
-            tile1.classList.remove('flipped'); //takes away flipped class
-            tile2.classList.remove('flipped'); // for tile 2
-            tile1.querySelector('span').classList.add('hidden'); // adds back hidden to the span element
-            tile2.querySelector('span').classList.add('hidden'); // for tile 2
-        }, 1000); // length of timeout remember in ms
+            tile1.classList.remove('flipped');
+            tile2.classList.remove('flipped');
+            tile1.querySelector('span').classList.add('hidden'); 
+            tile2.querySelector('span').classList.add('hidden'); 
+        }, 1000);
     }
 
-    flippedTiles = [];// reseting flipped tiles. fixes bug
+    flippedTiles = [];
 }
 
+const resetButton = document.getElementById('resetGame');
+const newGameButton = document.getElementById('newGame');
 
-// need to add reset button
-// need to add new game
-// need to add tutorial button
-// need to add home button
-//maybe make a slide bar for how many squares you want. DO THIS LAST IF DECIDED
+resetButton.addEventListener('click', resetGame);
+newGameButton.addEventListener('click', startGame);
+
+startGame();
