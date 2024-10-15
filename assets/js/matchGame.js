218fbea1cfd072c4a4ea66231236b73da0db74b0
Jordan Lorts <jordanjlorts@gmail.com>
2024-10-14 20:01:08 -0500

    cleaning up

--- assets/js/matchGame.js
+++ assets/js/matchGame.js
@@ -1,55 +1,127 @@
+const NUMBER_TILES = 16;
+
 const gameBoard = document.getElementById('game-board');
-let tiles = [];
-let flippedTiles = [];
-let matchedPairs = 0;
-const tileValues = [];
+const tiles = [];
+const flippedTiles = [];
 
-for (let i = 1; i <= 8; i++) {
-    tileValues.push(i, i);
-}
-
-function shuffle(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
+const currentGame = (() => {
+    const tileValues = [];
+    const matchedPairs = [];
+    const saveGame = () => {
+        const curGameHelper = {
+            tileValues,matchedPairs
+        }
+        localStorage.setItem('matchGameSaveData', JSON.stringify(curGameHelper));
     }
-    return array;
+    const loadGame = () => {
+        const data = JSON.parse(localStorage.getItem('matchGameSaveData')) || null;
+
+        if (data === null){
+            setEmptyGame();
+        }else{
+            tileValues.push(...data.tileValues);
+            matchedPairs.push(...data.matchedPairs);
+        }
+        return [tileValues,matchedPairs];
+    }
+    const setNewGame = (tileValueArr) => {
+        tileValues.length = 0;
+        tileValues.push(...tileValueArr);
+        matchedPairs.length = 0;
+        saveGame();
+    }
+    const setEmptyGame = () => {
+        tileValues.length = 0;
+        matchedPairs.length = 0;
+        saveGame();
+    }
+    const addMatchedPair = (pairNum) => {
+        matchedPairs.push(pairNum);
+        saveGame();
+    }
+    return {
+        getTileValues: () => [...tileValues], 
+        getMatchedPairs: () => [...matchedPairs], 
+        loadGame,
+        setNewGame,
+        setEmptyGame,
+        addMatchedPair
+    }
+})();
+
+
+/* UI Functions */
+const setTileFlipped = (tile) => {
+    tile.classList.add('flipped');
+    tile.querySelector('span').classList.remove('hidden');
+}
+const setTilesMatched = (tiles) => {
+    tiles.forEach(tile => tile.classList.add('matched'));
+}
+const setTilesHidden = (tiles) => {
+    tiles.forEach(tile => {
+        tile.classList.remove('flipped');
+        tile.querySelector('span').classList.add('hidden'); 
+    });
 }
 
-function startGame() {
-    shuffle(tileValues);
-    matchedPairs = 0; 
-    tiles.forEach(tile => gameBoard.removeChild(tile)); 
-    tiles = []; 
+const setUI = (tileValues,matchedTiles) => {
+    console.log('before: ', gameBoard.innerHTML);
+    gameBoard.innerHTML = '';
+    tiles.length = 0;
+    console.log('after: ', gameBoard.innerHTML);
 
-    for (let value of tileValues) {
+    for (let i = 0; i < tileValues.length; i++) {
         const tile = document.createElement('div');
         tile.classList.add('tile');
-        tile.dataset.value = value;
-        tile.innerHTML = '<span class="hidden">' + value + '</span>'; 
+        tile.dataset.value = tileValues[i];
+        tile.innerHTML = '<span class="hidden">' + tileValues[i] + '</span>'; 
         tile.addEventListener('click', flipTile);
         tiles.push(tile);
         gameBoard.appendChild(tile);
     }
+    console.log(gameBoard.children.length);
 }
 
-function resetGame() {
-    matchedPairs = 0;
-    flippedTiles = []; 
-    tiles.forEach(tile => {
-        tile.classList.remove('flipped', 'matched');
-        tile.querySelector('span').classList.add('hidden'); 
-    });
+/* Game Logic */
+const generateBoard = (numTiles) => {
+    if(numTiles % 2 !== 0) {
+        console.error('Number of tiles must be even');
+        return null;
+    }
+
+    const tileValues = [];
+    for (let i = 1; i <= numTiles/2; i++) {
+        tileValues.push(i, i);
+    }
+
+    for (let i = tileValues.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [tileValues[i], tileValues[j]] = [tileValues[j], tileValues[i]];
+    }
+    return tileValues;
+}
+
+const startGame = (numTiles) =>{
+    const tileValues = generateBoard(numTiles);
+    currentGame.setNewGame(tileValues);
+    flippedTiles.length = 0;
+
+    console.log(`generated new game with ${tileValues} tiles`);
+
+    setUI(tileValues);
 }
 
 function flipTile() {
     if (flippedTiles.length === 2 || this.classList.contains('flipped') || this.classList.contains('matched')) {
         return;
     }
+
+    console.log('hello, who is this: ', this);
     
-    this.classList.add('flipped');
-    this.querySelector('span').classList.remove('hidden');
     flippedTiles.push(this);
+
+    setTileFlipped(this);
 
     if (flippedTiles.length === 2) {
         checkForMatch();
@@ -60,28 +132,39 @@ function checkForMatch() {
     const [tile1, tile2] = flippedTiles;
 
     if (tile1.dataset.value === tile2.dataset.value) {
-        tile1.classList.add('matched');
-        tile2.classList.add('matched');
-        matchedPairs++;
-        if (matchedPairs === 8) {
-            alert('You won! All pairs matched.');
+        setTilesMatched([tile1, tile2]);
+        currentGame.addMatchedPair(tile1.dataset.value);
+        if (currentGame.getMatchedPairs() === NUMBER_TILES/2) {
+            handleGameOver(true);
         }
     } else {
         setTimeout(() => {
-            tile1.classList.remove('flipped');
-            tile2.classList.remove('flipped');
-            tile1.querySelector('span').classList.add('hidden'); 
-            tile2.querySelector('span').classList.add('hidden'); 
+            setTilesHidden([tile1, tile2]);
         }, 1000);
     }
 
-    flippedTiles = [];
+    flippedTiles.length = 0;
 }
 
-const resetButton = document.getElementById('resetGame');
+const handleGameOver = (isWin) => {
+    if (isWin) {
+        console.log('You win!');
+    } else {
+        console.log('You lose!');
+    }
+    resetGame();
+}
+
 const newGameButton = document.getElementById('newGame');
+newGameButton.addEventListener('click', () => startGame(NUMBER_TILES));
 
-resetButton.addEventListener('click', resetGame);
-newGameButton.addEventListener('click', startGame);
+const initGame = () => {
+    const [tileValues,matchedPairs] = currentGame.loadGame();
+    if (tileValues.length === 0) {
+        startGame(NUMBER_TILES);
+    } else {
+        setUI(tileValues,matchedPairs);
+    }
+}
 
-startGame();
+initGame();
