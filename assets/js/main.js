244d5ff71c3751dca2e8abea3bc0379dfae0aa79
Jason Cable <cablej02@gmail.com>
2024-10-20 16:21:38 -0500

    updated home page styling

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,6 +11,16 @@ const Color = {
 const tooltipTriggerList = [...document.querySelectorAll('[data-bs-toggle="tooltip"]')];
 const tooltipList = tooltipTriggerList.map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
 
+
+//Home page zoom out
+const heroImage = document.querySelector('.hero-image');
+document.addEventListener('scroll', function () {
+    const scrollPosition = window.pageYOffset;
+    console.log(scrollPosition);
+    heroImage.style.transform = `scale(${1 + scrollPosition / 1000})`;
+});
+
+
 // ! Use the following function whenever you need to redirect to a different page
 let redirectURL = '';
 
